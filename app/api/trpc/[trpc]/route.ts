467e009af3fb69ea23da8c3538b59bedc7a664b6
Rayyan-Shk <rayyanshaikhassociates@gmail.com>
2025-02-24 23:08:04 +0530

    fix: trpc api

--- app/api/trpc/[trpc]/route.ts
+++ app/api/trpc/[trpc]/route.ts
@@ -1,8 +1,8 @@
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
-import { appRouter } from "@/src/server/trpc/router/_app"; // Ensure this path is correct
+import { appRouter } from "@/src/server/trpc/router/_app"
 import { createContext } from "@/src/server/trpc/trpc";
 
-export const runtime = "nodejs"; // Ensure correct runtime for edge/serverless
+export const runtime = "nodejs";
 
 export async function POST(req: Request) {
   return fetchRequestHandler({
