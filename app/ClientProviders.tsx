cc896ff2ea87ed40990f0b59a9ab88fe568d1955
Rayyan-Shk <rayyanshaikhassociates@gmail.com>
2025-02-24 22:04:02 +0530

    added: providers

---
+++ app/ClientProviders.tsx
@@ -0,0 +1,33 @@
+// app/ClientProviders.tsx
+"use client";
+import * as React from "react";
+import { trpc } from "@/src/utils/trpc";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { httpBatchLink } from "@trpc/client";
+
+export default function ClientProviders({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [queryClient] = React.useState(() => new QueryClient());
+  const [trpcClient] = React.useState(() =>
+    trpc.createClient({
+      links: [
+        httpBatchLink({
+          url: "/api/trpc",
+        }),
+      ],
+    })
+  );
+
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </trpc.Provider>
+  );
+}
